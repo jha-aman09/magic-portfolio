e7772ebf0a71dcd81bab528a5f2c747bad973d29
Aman Jha <pjha78498@gmail.com>
2025-02-05 15:13:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,12 +21,7 @@ export async function generateMetadata() {
       description,
       type: "website",
       url: `https://${baseURL}`,
-      images: [
-        {
-          url: ogImage,
-          alt: title,
-        },
-      ],
+      images: [],
     },
     twitter: {
       card: "summary_large_image",
